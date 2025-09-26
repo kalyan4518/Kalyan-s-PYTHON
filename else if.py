d0c2454f185044f208b523122faf2903fc1d4628
VITTHANALA KALYAN VENKAT <kalyanvv21@gmail.com>
2025-09-26 13:15:38 +0530

    Add files via upload

---
+++ else if.py
@@ -0,0 +1,13 @@
+marks=int(input("ENTER THE MARKS"))
+if(marks>90):
+    print("A+")
+elif(marks>80):
+    print("B")
+elif(marks>70):
+    print("C")
+elif(marks>60):
+    print("D")
+elif(marks>50):
+    print("E")
+else:
+    print("FAIL")
